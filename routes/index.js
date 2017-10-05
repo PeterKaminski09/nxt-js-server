b98e9c8c8c4ba29aee23179efa8c2fd5f551d6a7
PeterKaminski09 <Peter.Kaminski09@gmail.com>
2017-10-05 13:24:50 -0700

    Test route

--- routes/index.js
+++ routes/index.js
@@ -16,7 +16,7 @@ router.get('/test', function(req, res, next){
     socket.emit('play sound', { data: 'hello'}); //Just send a test signal out
   });
 
-  res.send({ socketio: val, socketBank: socketBank});
+  res.send({ socketio: val, socketBank: 'success'});
 });
 
 router.get('/code/:macAddress', function(req, res, next) {
