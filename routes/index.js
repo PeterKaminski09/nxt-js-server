a1676b73e3b79460d392e605270b0185c002ed7d
PeterKaminski09 <Peter.Kaminski09@gmail.com>
2017-10-12 11:33:44 -0700

    Update the server to test the test route

--- routes/index.js
+++ routes/index.js
@@ -16,7 +16,7 @@ router.get('/test', function(req, res, next){
     socket.emit('play sound', { data: 'hello'}); //Just send a test signal out
   });
 
-  res.send({ socketio: val, connections: _.values(socketBank).length});
+  res.send({ socketio: val, connections: _.values(socketBank).length, ads: Object.keys(socketBank)});
 });
 
 router.get('/code/:macAddress', function(req, res, next) {
